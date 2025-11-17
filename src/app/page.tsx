8f2168b47e10671352eac0f4859cfd128e783587
Franco Serjen <francoserjen100@gmail.com>
2025-11-17 12:08:17 -0300

    Clean Code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,9 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        
-        <div>
-          <h1>Page</h1>
-        </div>
-        
-      </main>
-    </div>
+    <>
+      <div>
+      </div>
+    </>
   );
 }
